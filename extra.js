5ce6dcab1307d137886e80030f1d061af4d3e5bf
hagar-dolev <hagar.dolev@gmail.com>
2020-01-29 18:26:47 +0200

    site

---
+++ extra.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', () => {
+  const cursor = document.querySelectorAll('.cursor')[0];
+  const parent = document.querySelectorAll('.article')[0];
+  const cursorOffsetRelativeToParent = cursor.offsetLeft - parent.offsetLeft;
+  
+  const spacer = document.querySelectorAll('.spacer')[0];
+  console.log(spacer);
+  spacer.style.width = `${cursorOffsetRelativeToParent}px`;
+});
+
+var c,ctx;
+onload=setInterval(randomize,1000);
+
+function to generate random circle parameters, x,y and radius
+
+function randomize(){
+ // console.log("here");
+  c = document.getElementById("canvas");
+  ctx = c.getContext("2d");
+  var rr = Math.ceil((30* Math.random())+5);
+  var rx = Math.ceil(290* Math.random());
+  var ry = Math.ceil(290* Math.random());
+  drawCircle(rx,ry,rr);  
+}
+function drawCircle(rx,ry,rr){
+  var myColors  =["blue","red","green","yellow"];
+  var colorPicker = Math.ceil(4* Math.random() -1);
+  ctx.strokeStyle = myColors[colorPicker];
+  ctx.beginPath();
+  ctx.arc(rx,ry,rr,0,2*Math.PI);
+  ctx.stroke();
+  ctx.closePath();
+}
+
